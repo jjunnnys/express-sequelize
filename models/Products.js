d3043c602b8a7a21efd98e500ed6e51ee7382f14
ppby <salswnss@gmail.com>
2020-06-20 22:23:22 +0900

    Update moment

--- models/Products.js
+++ models/Products.js
@@ -2,6 +2,8 @@
     create table Products
 */
 
+const moment = require('moment');
+
 module.exports = (sequelize, DataTypes) => {
   const Products = sequelize.define('Products', {
     id: {
@@ -19,5 +21,11 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.TEXT,
     },
   });
+
+  // 함수를 추가하는 것을 프로토타입이라고 함, dataFormat이라는 함수를 만듦
+  Products.prototype.dateFormat = (date) => {
+    return moment(date).format('YYYY년 MM월 DD일');
+  };
+
   return Products;
 };
