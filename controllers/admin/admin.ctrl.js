6f0171325f670c17e887406b2b329c0763be4ac8
ppby <salswnss@gmail.com>
2020-06-20 21:30:02 +0900

    Update DB search

--- controllers/admin/admin.ctrl.js
+++ controllers/admin/admin.ctrl.js
@@ -1,10 +1,18 @@
 const model = require('../../models');
 
 exports.get_products = (_, res) => {
-  res.render(
-    'admin/products.html',
-    { message: 'hello' } // message 란 변수를 템플릿으로 내보낸다.
-  );
+  //   res.render(
+  //     'admin/products.html',
+  //     { message: 'hello' } // message 란 변수를 템플릿으로 내보낸다.
+  //   );
+  // 죄회하기 findAll
+  model.Products.findAll({
+    // 결과를 다음 then의 프롭스로 준다.
+  }).then((productList) => {
+    res.render('admin/products.html', {
+      productList,
+    });
+  });
 };
 
 exports.get_products_write = (_, res) => {
@@ -15,11 +23,17 @@ exports.get_products_write = (_, res) => {
 exports.post_products_write = (req, res) => {
   // res.send(req.body);
   // 필드 맞추기
-  model.Products.create({
+  model.Products.create(
+    /* 
+    {
     name: req.body.name,
     price: req.body.price,
     description: req.body.description,
-  }).then(() => {
+    }
+    이거랑 같다.
+    */
+    req.body
+  ).then(() => {
     // 저장 후 메인페이지로 redirect
     res.redirect('/admin/products');
   });
