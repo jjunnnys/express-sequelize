c4413370b4dfb62c78d5f5e8da94d204d654ae0e
ppby <salswnss@gmail.com>
2020-06-21 14:59:41 +0900

    Update async/await

--- controllers/admin/admin.ctrl.js
+++ controllers/admin/admin.ctrl.js
@@ -1,81 +1,67 @@
 const models = require('../../models');
 
-exports.get_products = (_, res) => {
-  //   res.render(
-  //     'admin/products.html',
-  //     { message: 'hello' } // message 란 변수를 템플릿으로 내보낸다.
-  //   );
-  // 죄회하기 findAll
-  models.Products.findAll({
-    // 결과를 다음 then의 프롭스로 준다.
-  }).then((productList) => {
+exports.get_products = async (_, res) => {
+  try {
+    const products = await models.Products.findAll({});
     res.render('admin/products.html', {
-      productList,
+      products,
     });
-  });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 exports.get_products_write = (_, res) => {
   res.render('admin/write.html');
 };
 
-// INSERT 하기 (sequelize에선 create)
-exports.post_products_write = (req, res) => {
-  // res.send(req.body);
-  // 필드 맞추기
-  models.Products.create(
-    /* 
-    {
-    name: req.body.name,
-    price: req.body.price,
-    description: req.body.description,
-    }
-    이거랑 같다.
-    */
-    req.body
-  ).then(() => {
-    // 저장 후 메인페이지로 redirect
+exports.post_products_write = async (req, res) => {
+  try {
+    await models.Products.create(req.body);
     res.redirect('/admin/products');
-  });
+  } catch (error) {
+    console.log(error);
+  }
 };
-// req.params.id 로 받을 수 있음
 
-exports.get_products_detail = (req, res) => {
-  models.Products.findByPk(req.params.id).then((product) => {
+exports.get_products_detail = async (req, res) => {
+  try {
+    const product = await models.Products.findByPk(req.params.id);
     res.render('admin/detail.html', { product });
-  });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-exports.get_products_edit = (req, res) => {
-  //기존에 폼에 value안에 값을 셋팅하기 위해 만든다.
-  models.Products.findByPk(req.params.id).then((product) => {
-    res.render('admin/write.html', { product: product });
-  });
+exports.get_products_edit = async (req, res) => {
+  try {
+    const product = await models.Products.findByPk(req.params.id);
+    res.render('admin/write.html', { product });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-exports.post_products_edit = (req, res) => {
-  models.Products.update(
-    // 데이터
-    {
-      name: req.body.name,
-      price: req.body.price,
-      description: req.body.description,
-    },
-    // 조건을 작성
-    {
+exports.post_products_edit = async (req, res) => {
+  try {
+    await models.Products.update(req.body, {
       where: { id: req.params.id },
-    }
-  ).then(() => {
+    });
     res.redirect(`/admin/products/detail/${req.params.id}`);
-  });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-exports.get_products_delete = (req, res) => {
-  models.Products.destroy({
-    where: {
-      id: req.params.id,
-    },
-  }).then(() => {
+exports.get_products_delete = async (req, res) => {
+  try {
+    await models.Products.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
     res.redirect('/admin/products');
-  });
+  } catch (error) {
+    console.log(error);
+  }
 };
